285cffe855c4f266a07a8199d48fc1e57d601501
hafsa61 <hafsayounis61@gmail.com>
2025-02-26 16:30:38 -0800

    modified

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     return (
         <div className='p-4'>
             <div className='flex justify-between items-center'>
-                <h1 className='text-3xl my-8'>Books List</h1>
+                <h1 className='text-3xl my-8'>Books Q</h1>
                 <Link to='/books/create'>
                     <MdOutlineAddBox className='text-sky-800 text-4xl'></MdOutlineAddBox>
                 </Link>
